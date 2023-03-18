3ff5a9acbf2e4a97bf47bec41b6f236c376327a7
ha.thi.tuoi <hatuoi958@gmail.com>
2023-03-18 15:18:21 +0700

    start

---
+++ src/main/java/Tuoi2403/Main.java
@@ -0,0 +1,7 @@
+package Tuoi2403;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/Tuoi2403/Outlook.java
@@ -0,0 +1,4 @@
+package Tuoi2403;
+
+public class Outlook {
+}
