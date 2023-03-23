99a0c9c4afd522b8686db4c49c8ef64695f3d315
ha.thi.tuoi <hatuoi958@gmail.com>
2023-03-23 10:22:41 +0700

    PR 23/03

--- src/main/java/Tuoi2403/Outlook.java
+++ src/main/java/Tuoi2403/Outlook.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 public class Outlook {
@@ -21,8 +23,10 @@ public class Outlook {
 
     @Test
     public void LoginPageTest() throws InterruptedException {
-        webDriver.get("https://login.live.com/login.srf");
+        webDriver.get("https://outlook.live.com/owa/");
         WebDriverWait wait = new WebDriverWait(webDriver, 10);
+        webDriver.findElement(By.xpath("(//a[@class = 'internal sign-in-link' and text()= 'Sign in'])[1]")).click();
+        Thread.sleep(2000);
         WebElement emailElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@type='email']")));
         emailElement.sendKeys("hatuoi958@outlook.com");
         WebElement nextButton = webDriver.findElement(By.xpath("//*[@id='idSIButton9']"));
@@ -33,6 +37,10 @@ public class Outlook {
         WebElement signinButton = webDriver.findElement(By.xpath("//*[@id='idSIButton9']"));
         signinButton.click();
         Thread.sleep(10000);
+        webDriver.findElement(By.xpath("//input[@type='submit']")).click();
+//        webDriver.get("https://login.live.com/ppsecure/post.srf");
+//        SessionId sessionId =((RemoteWebDriver) webDriver).getSessionId();
+//        System.out.println("Session ID : "+ sessionId);
     }
 
     @After
